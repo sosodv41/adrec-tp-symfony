e12fd25014473272c2d1f8fea92db84cfa2dc58d
Damien Citaire <damien5k@technodrome.lan>
2021-01-16 17:20:20 +0100

    part2: add Bootstrap and configure webpack, create header ans footer

---
+++ adrec_tp_symfony/src/Controller/HomeController.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class HomeController extends AbstractController
+{
+
+    /**
+     * @Route(path="/", name="home_page")
+     * @return Response
+     */
+    public function homePage(): Response
+    {
+        return $this->render('pages/home.html.twig', [
+            'date' => new \DateTime(),
+        ]);
+    }
+}
--- adrec_tp_symfony/webpack.config.js
+++ adrec_tp_symfony/webpack.config.js
@@ -20,7 +20,8 @@ Encore
      * Each entry will result in one JavaScript file (e.g. app.js)
      * and one CSS file (e.g. app.css) if your JavaScript imports CSS.
      */
-    .addEntry('app', './assets/app.js')
+    .addEntry('scripts', './assets/scripts.js')
+    .addEntry('styles', './assets/styles.scss')
 
     // enables the Symfony UX Stimulus bridge (used in assets/bootstrap.js)
     .enableStimulusBridge('./assets/controllers.json')
@@ -56,7 +57,7 @@ Encore
     })
 
     // enables Sass/SCSS support
-    //.enableSassLoader()
+    .enableSassLoader()
 
     // uncomment if you use TypeScript
     //.enableTypeScriptLoader()
