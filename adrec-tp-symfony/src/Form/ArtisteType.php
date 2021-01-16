b7bffa5068e2ee79c13d9dbfe56a8d9570aa1922
Blanchet Lydie <lyblanchet@orange.fr>
2021-01-16 18:39:58 +0100

    creation-des-crud

---
+++ adrec-tp-symfony/src/Form/ArtisteType.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\Artiste;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class ArtisteType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder
+            ->add('name')
+            ->add('nationality')
+            ->add('beginning_year')
+        ;
+    }
+
+    public function configureOptions(OptionsResolver $resolver)
+    {
+        $resolver->setDefaults([
+            'data_class' => Artiste::class,
+        ]);
+    }
+}
