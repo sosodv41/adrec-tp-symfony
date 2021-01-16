2eaf4e002262e70be49fbd67df47d316957a6347
Blanchet Lydie <lyblanchet@orange.fr>
2021-01-16 16:14:07 +0100

    ajout_des_entités

---
+++ adrec-tp-symfony/src/Entity/Artiste.php
@@ -0,0 +1,75 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\ArtisteRepository;
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * @ORM\Entity(repositoryClass=ArtisteRepository::class)
+ */
+class Artiste
+{
+    /**
+     * @ORM\Id
+     * @ORM\GeneratedValue
+     * @ORM\Column(type="integer")
+     */
+    private $id;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $name;
+
+    /**
+     * @ORM\Column(type="string", length=255, nullable=true)
+     */
+    private $nationality;
+
+    /**
+     * @ORM\Column(type="integer", nullable=true)
+     */
+    private $beginning_year;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getName(): ?string
+    {
+        return $this->name;
+    }
+
+    public function setName(string $name): self
+    {
+        $this->name = $name;
+
+        return $this;
+    }
+
+    public function getNationality(): ?string
+    {
+        return $this->nationality;
+    }
+
+    public function setNationality(?string $nationality): self
+    {
+        $this->nationality = $nationality;
+
+        return $this;
+    }
+
+    public function getBeginningYear(): ?int
+    {
+        return $this->beginning_year;
+    }
+
+    public function setBeginningYear(?int $beginning_year): self
+    {
+        $this->beginning_year = $beginning_year;
+
+        return $this;
+    }
+}
