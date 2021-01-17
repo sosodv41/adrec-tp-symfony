3e4e70a4028d57dcd299014deeb1aade21ef453a
Damien Citaire <damien5k@technodrome.lan>
2021-01-17 11:28:33 +0100

    part5: add artists list classed by nationality to HomePage

--- adrec_tp_symfony/src/Controller/HomeController.php
+++ adrec_tp_symfony/src/Controller/HomeController.php
@@ -3,6 +3,7 @@
 namespace App\Controller;
 
 use App\Repository\AlbumRepository;
+use App\Repository\ArtistRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
@@ -12,13 +13,18 @@ class HomeController extends AbstractController
 
     /**
      * @Route(path="/", name="home_page")
+     * @param ArtistRepository $artistRepository
      * @param AlbumRepository $albumRepository
      * @return Response
      */
-    public function homePage(AlbumRepository $albumRepository): Response
+    public function homePage2(
+        ArtistRepository $artistRepository,
+        AlbumRepository $albumRepository
+    ): Response
     {
         return $this->render('home.html.twig', [
             'albums' => $albumRepository->findAll(),
+            'artists' => $artistRepository->findByNationality(),
         ]);
     }
 }
