4c262871f89045a9465cb42317b9875a3edffb30
Karim HADJ-ABDELKADER <karim-hadj@hotmail.fr>
2021-01-23 23:51:15 +0100

    Initial commit

---
+++ tp-symfony/src/Controller/HomeController.php
@@ -0,0 +1,14 @@
+<?php
+
+
+class HomeController extends \Symfony\Bundle\FrameworkBundle\Controller\AbstractController
+{
+    /**
+     * @\Symfony\Component\Routing\Annotation\Route(path="/", name="home_page")
+     * @return \Symfony\Component\HttpFoundation\Response
+     */
+    public function homePage() : \Symfony\Component\HttpFoundation\Response
+    {
+
+    }
+}
